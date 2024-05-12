e8634aa26e7d4371c19cee9398310c2b8441e5f0
OutRed <103283478+OutRed@users.noreply.github.com>
2024-05-12 00:10:49 -0500

    update nav and styles

--- js/navbar.js
+++ js/navbar.js
@@ -1,29 +1,28 @@
-// Function to create the navigation bar
 function createNavBar() {
-    // Create the navigation bar container
     const navBar = document.createElement('div');
     navBar.className = 'navigation-bar';
 
-    // Create the navigation links
+    const title = document.createElement('div');
+    title.className = 'title-nav';
+    // title.textContent = 'O'; */
+
+    navBar.appendChild(title);
+
     const homeLink = createNavLink('Home', '/index.html', true);
     const gamesLink = createNavLink('Games', '/g/index.html');
     const appsLink = createNavLink('Apps', '/apps.html');
     const settingsLink = createNavLink('Settings', '/settings/index.html');
 
-    // Append the navigation links to the navigation bar
     navBar.appendChild(homeLink);
     navBar.appendChild(gamesLink);
     navBar.appendChild(appsLink);
     navBar.appendChild(settingsLink);
 
-    // Get the target element where the navigation bar will be added
     const targetElement = document.getElementById('navbar');
 
-    // Add the navigation bar to the target element
     targetElement.appendChild(navBar);
 }
 
-// Function to create a navigation link
 function createNavLink(text, href, isActive = false) {
     const link = document.createElement('p');
     link.textContent = text;
@@ -36,5 +35,4 @@ function createNavLink(text, href, isActive = false) {
     return link;
 }
 
-// Call the function to create the navigation bar
 createNavBar();
