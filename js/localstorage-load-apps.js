3746f9a349e92578fb4310f9faa21e0890b4ac3a
OutRed <103283478+OutRed@users.noreply.github.com>
2024-05-16 13:12:49 -0500

    r

--- js/localstorage-load-apps.js
+++ js/localstorage-load-apps.js
@@ -1,7 +1,9 @@
-if (!localStorage.getItem('currentapp')) location.pathname = '/load-app.html';
-document.getElementById('game-frame').src = localStorage.getItem("currentapp");
-document.getElementById('g-name').textContent = localStorage.getItem("currenttitle");
-document.getElementById('g-description').textContent = localStorage.getItem("currentdescription"); 
+if (!localStorage.getItem("currentapp")) location.pathname = "/load-app.html";
+document.getElementById("game-frame").src = localStorage.getItem("currentapp");
+document.getElementById("g-name").textContent =
+  localStorage.getItem("currenttitle");
+document.getElementById("g-description").textContent =
+  localStorage.getItem("currentdescription");
 /*
 const iframe = document.getElementById('game-frame');
 iframe.src = /*$__uvconfig.encode ('https://example.com};'); // put the uv encode function in place of the encode url function
--- js/localstorage-load.js
+++ js/localstorage-load.js
@@ -1,4 +1,6 @@
-if (!localStorage.getItem('currentgame')) location.pathname = '/g/load.html';
-document.getElementById('game-frame').src = localStorage.getItem("currentgame");
-document.getElementById('g-name').textContent = localStorage.getItem("currenttitle");
-document.getElementById('g-description').textContent = localStorage.getItem("currentdescription");
+if (!localStorage.getItem("currentgame")) location.pathname = "/g/load.html";
+document.getElementById("game-frame").src = localStorage.getItem("currentgame");
+document.getElementById("g-name").textContent =
+  localStorage.getItem("currenttitle");
+document.getElementById("g-description").textContent =
+  localStorage.getItem("currentdescription");
